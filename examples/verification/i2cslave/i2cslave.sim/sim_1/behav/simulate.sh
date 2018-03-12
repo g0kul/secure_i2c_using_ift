809d0a1006cbf416c708b938beedb271e75ebc75
GokulPrasath <gokulprasath@umail.ucsb.edu>
2018-03-12 13:34:42 -0700

    Updated waveform db files to results

--- examples/verification/i2cslave/i2cslave.sim/sim_1/behav/simulate.sh
+++ examples/verification/i2cslave/i2cslave.sim/sim_1/behav/simulate.sh
@@ -9,4 +9,4 @@ then
 exit $RETVAL
 fi
 }
-ExecStep $xv_path/bin/xsim i2c_sys_tb_behav -key {Behavioral:sim_1:Functional:i2c_sys_tb} -tclbatch i2c_sys_tb.tcl -view /home/gokul/playground/secverilog/i2c_ift/examples/verification/i2cslave/results/tst_bench_flow_det.wcfg -log simulate.log
+ExecStep $xv_path/bin/xsim i2c_sys_tb_behav -key {Behavioral:sim_1:Functional:i2c_sys_tb} -tclbatch i2c_sys_tb.tcl -log simulate.log
